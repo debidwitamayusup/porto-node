3f406474ee6d5a1aa179d8455877393991c486ac
debidwitamayusup <dwittamma19@gmail.com>
2025-04-08 16:12:58 +0700

    initial

---
+++ src/content_option.js
@@ -0,0 +1,196 @@
+const logotext = "SAMODOKSYNC";
+const meta = {
+    title: "samodoksync",
+    description: "I’m Samodoksync _ Full stack devloper,currently working in Jakarta",
+};
+
+const introdata = {
+    title: "I’m Debi Dwitama Yusup a.k.a. Samodoksync",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: `Samodoksync comes from "Samodoksingsang" means coconut meat that is just "bjirrr pas bangetttt" means 
+                    the meat is not too thin or thick, this word is sundanese word.
+                    the philosophy that you come to the very right place if you are look up for cool apps builder`,
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: `Experienced Web Programmer with a demonstrated history of working in the information technology and services industry. 
+            Skilled in PHP, C# .NET, Application Programming Interfaces, SOA, Databases, ETL, Representational State Transfer (REST), 
+            and Program Development. Strong engineering professional with a Bachelor's degree focused in Informatics Engineering`,
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [
+    {
+        name: "PHP",
+        value: 100
+    },
+    {
+        name: "C#",
+        value: 90,
+    },
+    {
+        name: "DOTNET",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Dart (Flutter)",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "SQL Server",
+        value: 95,
+    },
+    {
+        name: "Oracle SQL",
+        value: 85,
+    },
+    {
+        name: "MySQL",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Website Design and Building",
+        description: "Build a responsive cool website.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Since i am using flutter, my mobile apps can run through two mainstream OS platform both iOS and Android.",
+    },
+    {
+        title: "Website Wedding Invitation Design",
+        description: "Website Wedding Invitation with unlimited consultation and personalization.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    instagram: "https://instagram.com/igdebi_",
+    github: "",
+    facebook: "",
+    linkedin: "",
+    twitter: "",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
